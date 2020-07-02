e39da64d2fc4be359550f02ea175eee7583adc84
Brice Boutet <boutetlb@gmail.com>
2020-07-01 23:46:49 -0400

    Updates

---
+++ precache-manifest.7b41f31942858c6a5bc503927a6605fa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e3aa30832e5c8d6e782239e7b66a33",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d30470beb1a93dc555d4",
+    "url": "/my-portfolio/static/css/main.ae74c49b.chunk.css"
+  },
+  {
+    "revision": "bd6c0a2a31104fdf1fd1",
+    "url": "/my-portfolio/static/js/2.047ffd46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.047ffd46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d30470beb1a93dc555d4",
+    "url": "/my-portfolio/static/js/main.2d721e18.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "d76329e805bd80b62c67c135a778240a",
+    "url": "/my-portfolio/static/media/Brice_Boutet.d76329e8.jpg"
+  },
+  {
+    "revision": "d50bc6c8214614e7aa619828f1dfe52e",
+    "url": "/my-portfolio/static/media/codingdesk.d50bc6c8.jpg"
+  },
+  {
+    "revision": "2da7b3c757ee0846d695b34c61d67ac6",
+    "url": "/my-portfolio/static/media/daily-carpool.2da7b3c7.gif"
+  },
+  {
+    "revision": "e9badfb219a035968bc08c576b488921",
+    "url": "/my-portfolio/static/media/developer-profil.e9badfb2.gif"
+  },
+  {
+    "revision": "3466cf71a464baf7afd9d71c704a7222",
+    "url": "/my-portfolio/static/media/employee-directory.3466cf71.gif"
+  },
+  {
+    "revision": "7f84de3163d48565a9397d21e3348343",
+    "url": "/my-portfolio/static/media/resume.7f84de31.pdf"
+  },
+  {
+    "revision": "1738edbb5898889098a9f55fe23c2785",
+    "url": "/my-portfolio/static/media/team-generator.1738edbb.gif"
+  },
+  {
+    "revision": "36964babcfcd9c3895b8c61e7951484f",
+    "url": "/my-portfolio/static/media/weather-dashboard.36964bab.gif"
+  }
+]);
