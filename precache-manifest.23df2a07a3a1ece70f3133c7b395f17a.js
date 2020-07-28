13fe3da21b667b94c7e2a91b7131e5b0f247704c
Brice Boutet <boutetlb@gmail.com>
2020-07-27 22:59:09 -0400

    Updates

---
+++ precache-manifest.23df2a07a3a1ece70f3133c7b395f17a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c42149881462f87a313e94e6842977",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "96dce0a7b2b786bcb2af",
+    "url": "/portfolio/static/css/main.2c7dd340.chunk.css"
+  },
+  {
+    "revision": "154913b996a3150f74d3",
+    "url": "/portfolio/static/js/2.fdb61cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fdb61cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dce0a7b2b786bcb2af",
+    "url": "/portfolio/static/js/main.4f6cf9e7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d76329e805bd80b62c67c135a778240a",
+    "url": "/portfolio/static/media/Brice_Boutet.d76329e8.jpg"
+  },
+  {
+    "revision": "d50bc6c8214614e7aa619828f1dfe52e",
+    "url": "/portfolio/static/media/codingdesk.d50bc6c8.jpg"
+  },
+  {
+    "revision": "3e2b9421ccf58324b1f5d9e61ea62242",
+    "url": "/portfolio/static/media/daily-carpool.3e2b9421.gif"
+  },
+  {
+    "revision": "e9badfb219a035968bc08c576b488921",
+    "url": "/portfolio/static/media/developer-profil.e9badfb2.gif"
+  },
+  {
+    "revision": "00ded0e302a4a3a3e34db1760804298f",
+    "url": "/portfolio/static/media/eat-da-burger.00ded0e3.gif"
+  },
+  {
+    "revision": "3466cf71a464baf7afd9d71c704a7222",
+    "url": "/portfolio/static/media/employee-directory.3466cf71.gif"
+  },
+  {
+    "revision": "c9540054f8710d74a6eb1ddee7d47ff4",
+    "url": "/portfolio/static/media/resume.c9540054.pdf"
+  },
+  {
+    "revision": "ab8cdc18b2e18896c485c122a927f113",
+    "url": "/portfolio/static/media/team-generator.ab8cdc18.gif"
+  },
+  {
+    "revision": "c138f388ef7ad86b01315aed695b4f68",
+    "url": "/portfolio/static/media/weather-dashboard.c138f388.gif"
+  }
+]);
